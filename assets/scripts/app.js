8ba4e9f21c429f4fdbaa82da440895e0a93c3f4b
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-07-13 19:56:29 +0300

    add some comments

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,19 @@
-alert('This works!');
+// let username = 'John Doe';
+// username = 'Abdo';
+// console.log(username);
+
+
+// const totalUsers = 100;
+// totalUsers = 101;
+// console.log(totalUsers); // Error: Assignment to constant variable.
+
+// naming convention
+// let user_name = 'John Doe'; // snake_case (not recommended)
+// let userName = 'John Doe'; // camelCase (recommended)
+// start with a letter, underscore, or dollar sign
+// let $userName = 'John Doe';
+// let _userName = 'John Doe';
+
+let currentResult; // we can declare a variable without assigning a value to it.
+currentResult = 0;
+
