437d6b2e8a528363ce77fd9040bb0bd744170b5d
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-07-18 21:13:55 +0300

    fix linting

--- call-back/app.js
+++ call-back/app.js
@@ -3,12 +3,12 @@ const clickableBtn = document.getElementById('clickable-btn');
 const messageInput = document.getElementById('click-message-input');
 
 function printMessage() {
-  const value = messageInput.value;
-  console.log(value || 'Clicked me!');
+    const value = messageInput.value;
+    console.log(value || 'Clicked me!');
 }
 
 function addListener() {
-  clickableBtn.addEventListener('click', printMessage);
+    clickableBtn.addEventListener('click', printMessage);
 }
 
 addListenerBtn.addEventListener('click', addListener);
