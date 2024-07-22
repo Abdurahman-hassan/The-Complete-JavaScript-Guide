619459951f32700f8389dcf5b83dae1d78e4f789
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-07-22 13:27:40 +0300

    copy by value and copy by reference

--- main-concepts/app.js
+++ main-concepts/app.js
@@ -15,10 +15,53 @@
 // console.log(name, hobbies);
 
 // greet();
-'use strict';
+// ********************************************************************************************************************
+// 'use strict';
+//
+// const userName = 'Max';
+// var undefined = 5;
+//
+// console.log(userName);
 
-const userName = 'Max';
-var undefined = 5;
+let name = 'Max';
+// name -> Max
+let anotherName = name;
+// anotherName -> Max
+// name = 'Manuel';
+// anotherName -> Max
+// we copy by value
 
-console.log(userName);
+let hobbies = ['Sports', 'Cooking'];
+// hobbies -> [Sports, Cooking]
+let moreHobbies = hobbies;
+// moreHobbies -> [Sports, Cooking]
+// hobbies.push('Programming');
+// moreHobbies -> [Sports, Cooking, Programming]
+// we copy by reference
 
+let person = {age: 30};
+// person -> {age: 30}
+let secondPerson = person;
+// secondPerson -> {age: 30}
+// person.age = 31;
+// secondPerson -> {age: 31}
+// we copy by reference
+
+// if we need to copy by value we can use the spread operator
+let thirdPerson = {...person};
+// thirdPerson -> {age: 31}
+person.age = 32;
+// thirdPerson -> {age: 31}
+// we copy by value
+
+let hobbies_list = ['Sports', 'Cooking'];
+let moreHobbies_list = [...hobbies_list];
+hobbies_list.push('Programming');
+console.log(hobbies_list); // -> [Sports, Cooking, Programming]
+console.log(moreHobbies_list); // -> [Sports, Cooking]
+// ********************************************************************************************************************
+
+const person1 = {age:30};
+const person2 = {age:30};
+// person1 === person2; // -> false
+// ********************************************************************************************************************
