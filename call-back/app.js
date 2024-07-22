b7342c6274eb385c02e3ae675bab38acec6dc065
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-07-22 13:39:01 +0300

    using Anonymous functions

--- call-back/app.js
+++ call-back/app.js
@@ -2,13 +2,29 @@ const addListenerBtn = document.getElementById('add-listener-btn');
 const clickableBtn = document.getElementById('clickable-btn');
 const messageInput = document.getElementById('click-message-input');
 
+
+let person = {name: 'Abdo', age: 30};
+// js garbage collector will delete the object from the memory if it's not used anymore
+person = null;
+
 function printMessage() {
     const value = messageInput.value;
     console.log(value || 'Clicked me!');
 }
 
 function addListener() {
+    console.log('Adding listener...');
     clickableBtn.addEventListener('click', printMessage);
 }
 
-addListenerBtn.addEventListener('click', addListener);
+// Anonymous function this behavior is called closure it's not a good practice to use it
+function addListener_using_anonmous_function() {
+    console.log('Adding listener...');
+    clickableBtn.addEventListener('click', function() {
+        const value = messageInput.value;
+        console.log(value || 'Clicked me!');
+    });
+}
+
+// addListenerBtn.addEventListener('click', addListener);
+addListenerBtn.addEventListener('click', addListener_using_anonmous_function);
