9561033dd6781e93a3fdf0a9c9952d5a7ad80440
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-07-26 23:16:16 +0300

    adding bind

--- main/app.js
+++ main/app.js
@@ -65,7 +65,7 @@ startGameBtn.addEventListener('click', function () {
         winner = getWinner(computerSelection, playerSelection); // if playerSelection is undefined, then we call getWinner with only one argument
     }
 
-    let message = `You picked ${playerSelection ||  DEFAULT_USER_CHOICE}, computer picked ${computerSelection}, therefore you `;
+    let message = `You picked ${playerSelection || DEFAULT_USER_CHOICE}, computer picked ${computerSelection}, therefore you `;
     if (winner === RESULT_DRAW) {
         message += 'had a draw.';
     } else if (winner === RESULT_PLAYER_WINS) {
@@ -181,35 +181,101 @@ startGameBtn.addEventListener('click', function () {
 // rest operator
 // the rest operator is used to merge a list of function arguments into an array
 // use callback function to handle the result
-const sumUp = (resultHandler, ...numbers) => {
+const combine = (resultHandler, operation, ...numbers) => {
     const validateNumber = (number) => {
         return isNaN(number) ? 0 : number;
     };
 
-    let sum = 0;
+    let sum;
+    if (operation === 'ADD') {
+        sum = 0;
+    } else if (operation === 'SUBTRACT' || operation === 'DIVIDE') {
+        sum = validateNumber(numbers[0]);
+        numbers = numbers.slice(1); // Remove the first number for the loop
+    } else if (operation === 'MULTIPLY') {
+        sum = 1;
+    }
+
     for (const num of numbers) {
-        sum += validateNumber(num);
+        if (operation === 'ADD') {
+            sum += validateNumber(num);
+        } else if (operation === 'SUBTRACT') {
+            sum -= validateNumber(num);
+        } else if (operation === 'MULTIPLY') {
+            sum *= validateNumber(num);
+        } else if (operation === 'DIVIDE') {
+            sum /= validateNumber(num);
+        } else {
+            sum += validateNumber(num);
+        }
     }
     resultHandler(sum); // execute the callback function with the result
 }
 
 
-const showResult = (result) => {
-    alert('The result after adding all numbers is: ' + result);
+const showResult = (messegaText, result) => {
+    alert(messegaText + ' ' + result);
 }
 
 
-sumUp(showResult, 2,2,2);
+
+// we used bind which allows to pre-cofigure
+// the functions in places where we need to pass
+// a value but we also don't want to directly execute the function
+combine(showResult.bind(this, 'The result after adding all numbers is:'), 'ADD', 10, 10);
+combine(showResult.bind(this, 'The result after subtracting all numbers is:'), 'SUBTRACT', 20, 10);
+combine(showResult.bind(this, 'The result after multiplying all numbers is:'), 'MULTIPLY', 10, 10);
+combine(showResult.bind(this, 'The result after dividing all numbers is:'), 'DIVIDE', 90, 10);
+
+
+/*
+What bind() Does Here
+Pre-configure Arguments:
+bind() is used to create a new version of showResult where the first argument (messageText) is already set. For example:
+
+
+showResult.bind(this, 'The result after adding all numbers is:')
+This creates a new function that, when called, will always use 'The result after adding all numbers is:' as the first argument.
+
+Context (this):
+Although this is not particularly relevant in this specific example, bind() also sets the value of this inside the new function.
+Here, it's set to this, which is usually the global object or the current context in which the code is running.
+
+Simplified Explanation
+bind() creates a new function.
+The new function has this set to a specified value.
+It also has pre-set arguments that are passed when the function is called.
+
+
+bind in situations where you want to pre-configure a function argument
+when you are not calling the function on your own but you want to pass it to another function
+
+
+ */
+
+
+
+
+// // we can use arguments object to get all arguments passed to a function,
+// // but it's not an array, it's an array-like object
+// const subtractUp = function () {
+//     let sum = 0;
+//     for (const num of arguments) { // not recommended to use arguments object
+//         sum -= num;
+//     }
+//     return sum;
+// }
 
 
 // we can use arguments object to get all arguments passed to a function,
 // but it's not an array, it's an array-like object
-const subtractUp = function () {
-    let sum = 0;
-    for (const num of arguments) { // not recommended to use arguments object
-        sum -= num;
-    }
-    return sum;
-}
-
-console.log(subtractUp(1, 10, 15, 20));
+// const subtractUp = function (resultHandler, ...numbers) {
+//     let sum = 0;
+//     for (const num of numbers) { // not recommended to use arguments object
+//         sum -= num;
+//     }
+//     resultHandler(sum);
+// }
+//
+//
+// console.log(subtractUp(showResult, 1, 10, 15, 20));
